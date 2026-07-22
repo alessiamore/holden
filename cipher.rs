1be912fc8e8fe130e849fad8958cb1b0074ad389
alessiamore <alessiamore@proton.me>
2026-07-22 22:37:58 +0300

    Create cipher.rs

---
+++ cipher.rs
@@ -0,0 +1,16 @@
+#[derive(Clone)]
+pub struct Cipher {
+
+    pub name:String,
+
+    pub year:u16,
+
+    pub category:String,
+
+    pub security:String,
+
+    pub description:String,
+
+    pub successor:String,
+
+}
